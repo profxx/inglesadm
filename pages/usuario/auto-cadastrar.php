a4ae5605f701f71f1b68d6e3b328b43312fd8abf
Alexandre Pimentel <acpimentel@gmail.com>
2024-08-24 22:18:59 -0300

    Start the self registration area

--- pages/usuario/auto-cadastrar.php
+++ pages/usuario/auto-cadastrar.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<link rel="icon" href="images/flag-icon.png" />
-    <title>Inglês ADM</title>
+    <title>Inglês ADM - Nova conta</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
@@ -23,56 +23,74 @@
 </head>
 <body>
 
-
 		<div class="card">
 		    
 		    
 			<div class="card-header">
-				<h3>Sign in</h3>
+				<h3>Sign up</h3>
 
 			</div>
 			
 			
 			<div class="card-body">
 				<form action="pages/usuario/validate.php" method="post">
-					<?php
-						if (isset($_GET['error'])){ ?>
-							<p class="error"> <?php echo $_GET['error']; ?> </p>
-						<?php } ?>
-
+                    
+                    <!-- Username -->
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
 						<input type="text" name="usu_login" class="form-control" placeholder="username">
-						
 					</div>
+
+                    <!-- E-mail address -->
 					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="usu_login" class="form-control" placeholder="e-mail address">
+					</div>
+
+                    <!-- Phone number -->
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="usu_login" class="form-control" placeholder="phone number">
+					</div>
+					
+                    <!-- Password -->
+                    <div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
 						<input type="password" name="usu_senha" class="form-control" placeholder="password">
 					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox" name="remember">Remember Me
+
+                    <!-- Confirm Password -->
+                    <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" name="usu_senha" class="form-control" placeholder="confirm password">
 					</div>
+
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+						<input type="submit" value="Register" class="btn float-right login_btn">
 					</div>
 				</form>
 			</div>
 			
 			
-			
+			<!-- Footer -->
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="./pages/usuario/auto-cadastrar.php">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
+					You'll be sent an e-mail to confirm your account. Check the spam folder.
 				</div>
 			</div>
-		</div>
+
+
+		</div> <!-- CARD END -->
 
 
 </body>
